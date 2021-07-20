2cd3450d9e51d264476f525f55a8a7cec904a456
github username <github email address>
2021-07-20 11:26:27 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function saturdayFun(funActivity = 'roller-skate') {
+    return `This Saturday, I want to ${funActivity}!`
+}
+function mondayWork(workActivity = 'go to the office') {
+    return `This Monday, I will ${workActivity}.`
+}
+function wrapAdjective(visualFlair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+wrapAdjective('!!!')('really cool')
